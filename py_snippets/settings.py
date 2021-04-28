355ea1c12bd5369c90e5676f55af4cca1d9ec801
Max Rokitsky <exenzi@gmail.com>
2021-04-28 23:41:51 +0300

    add env DJANGO_DEBUG

--- py_snippets/settings.py
+++ py_snippets/settings.py
@@ -12,9 +12,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DJANGO_DEBUG']
 
-ALLOWED_HOSTS = ['*']
+if DEBUG:
+    ALLOWED_HOSTS = ['*']
+else:
+    ALLOWED_HOSTS = os.environ['DJANGO_HOST']
 
 # Application definition
 
